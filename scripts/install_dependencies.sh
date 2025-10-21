b559210351f49f4347b5512b47f320157eda77f3
seminchoi-ss <sm.choi@smileshark.kr>
2025-10-21 15:22:18 +0900

    feat: cicd yml 전체 작성

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "Installing Node.js dependencies..."
-
 # Source nvm to get node/npm/pm2 on the PATH for ubuntu user
 export NVM_DIR="/home/ubuntu/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-cd /home/ubuntu/app-tier
-npm install --production
+echo "Process After Install..."
